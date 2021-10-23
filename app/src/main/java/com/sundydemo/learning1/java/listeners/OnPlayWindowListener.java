94a3f123dd0b0e75e29b2c22e0391f55c97203ed
sundy19 <57116184+sundy19@users.noreply.github.com>
2021-10-22 17:42:03 -0700

    Update OnPlayWindowListener.java

--- app/src/main/java/com/sundydemo/learning1/java/listeners/OnPlayWindowListener.java
+++ app/src/main/java/com/sundydemo/learning1/java/listeners/OnPlayWindowListener.java
@@ -22,7 +22,7 @@ import android.view.View.OnClickListener;
 
 /**
  * @since 2020
- * @author Huawei DTSE USA
+ * @author Huawei DTSE India
  */
 public interface OnPlayWindowListener
         extends Callback, OnClickListener, SurfaceTextureListener, OnItemClickListener, OnPlaySettingListener {
