dfcc75985108cbd4b87e49d7f48196e84ed9a9a5
sundy19 <57116184+sundy19@users.noreply.github.com>
2021-10-22 17:40:44 -0700

    Update OnDialogInputValueListener.java

--- app/src/main/java/com/sundydemo/learning1/java/listeners/OnDialogInputValueListener.java
+++ app/src/main/java/com/sundydemo/learning1/java/listeners/OnDialogInputValueListener.java
@@ -17,7 +17,7 @@ package com.sundydemo.learning1.java.listeners;
 
 /**
  * @since 2020
- * @author Huawei DTSE USA
+ * @author Huawei DTSE India
  */
 public interface OnDialogInputValueListener {
     /**
