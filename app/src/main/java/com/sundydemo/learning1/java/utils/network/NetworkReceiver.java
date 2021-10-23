edd9ac953911450b1f9d5eeba9be4742fc9c92de
sundy19 <57116184+sundy19@users.noreply.github.com>
2021-10-22 17:49:18 -0700

    Update NetworkReceiver.java

--- app/src/main/java/com/sundydemo/learning1/java/utils/network/NetworkReceiver.java
+++ app/src/main/java/com/sundydemo/learning1/java/utils/network/NetworkReceiver.java
@@ -24,7 +24,7 @@ import android.widget.Toast;
 
 /**
  * @since 2020
- * @author Huawei DTSE USA
+ * @author Huawei DTSE India
  */
 public class NetworkReceiver extends BroadcastReceiver {
     /**
