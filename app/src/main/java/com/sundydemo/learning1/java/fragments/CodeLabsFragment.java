7a630a0ae84c19a4c6cc3a5afa524ae105e3503e
sundy19 <57116184+sundy19@users.noreply.github.com>
2021-10-22 17:36:31 -0700

    Update CodeLabsFragment.java

--- app/src/main/java/com/sundydemo/learning1/java/fragments/CodeLabsFragment.java
+++ app/src/main/java/com/sundydemo/learning1/java/fragments/CodeLabsFragment.java
@@ -41,7 +41,7 @@ import java.util.Objects;
 
 /**
  * @since 2020
- * @author Huawei DTSE USA
+ * @author Huawei DTSE India
  */
 public class CodeLabsFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
